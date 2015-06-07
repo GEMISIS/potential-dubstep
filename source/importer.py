ab1b69f98861b84ed9b001513cbe2a5bbe1125ba
Jesse Harrison <jharrison902@gmail.com>
2015-06-07 11:17:22 -0400

    Added initial tiling work. See todo's for things left.

--- source/importer.py
+++ source/importer.py
@@ -23,7 +23,9 @@ class Importer:
         self._directory = ""
         self._satellite = None
         self._table = "GISBox"
-        self._projection_target = "EPSG:32618" #projection handy for the test region
+        self._projection_target_short = "EPSG:32618" #projection handy for the test region
+        self._projection_target = 'PROJCS["WGS 84 / UTM zone 18N",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433],AUTHORITY["EPSG","4326"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",-75],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AUTHORITY["EPSG","32618"]]'
+        self._tmp_dir = "/Users/jharrison902/Desktop/giswork/tmp"
         self._bands={}
         self._masks={}
     
@@ -86,7 +88,7 @@ class Importer:
                 self._masks[attributes['name']]={}
                 self._masks[attributes['name']]['bits']=[]
                 for bit in band.find('{http://espa.cr.usgs.gov/v1.2}bitmap_description'):
-                    self._masks[attributes['name']]['bits']=bit.attrib[num]
+                    self._masks[attributes['name']]['bits']=bit.attrib['num']
         
                 
                 
@@ -99,28 +101,102 @@ class Importer:
         """
         Set our importer to use Landsat 7 settings
         """
-        self._satellite = LandSat7()
+        self._satellite = 'LANDSAT_7'
+        """
+        TODO: should this do more?
+        """
         
     def set_satellite_landsat_8(self):
         """
         Set our importer to use Landsat 8 settings
         """
-        self._satellite = Landsat8()
+        self._satellite = 'LANDSAT_8'
+        """
+        TODO: should this do more?
+        """
     
-    def generate_cmd_string(self, tile_path, tile_x, tile_y):
+    def generate_cmd_string(self, input_file, tile_path, tile_x, tile_y, size):
         """
         Generate the string to store a tile in the database.
         """
+        cmd = 'gdalwarp'
+        cmd+=' -of GTiff' #geotiff output
+        cmd+=' -tr 30 30' #30m resolution
+        cmd+=' -te '+str(tile_x)+' '+str(tile_y)+' '+str(tile_x+size)+' '+str(tile_y+size)
+        cmd+=' -r cubic' #cubic resampling if we absolutely have to resample
+        cmd+=' '+os.path.join(self._directory,input_file)
+        cmd+=' '+os.path.join(self._tmp_dir,tile_path)
         
-        raise NotImplementedError
+        return cmd
         
-    def tile_raster(self,target_raster):
+    def tile_raster(self):
         """
         Convert raster into tiles using gdalwarp.
         Coordinates will be stored in self._projection_target projection.
         """
+        gdal.AllRegister()
+        for band in self._bands:
+            file_path = os.path.join(self._directory,band['file'])
+            band_file = gdal.Open(file_path)
+            """
+            TODO: implement a database lock on inserts at this point
+            """
+            """
+            Reproject our data
+            """
+            band_file.setProjection(self._projection_target)
+            band_file.FlushCache()
+            """
+            Slice data into tiles based on size in coordinate system
+            """
+            x_size = band_file.RasterXSize
+            y_size = band_file.RasterYSize
+            geotrans = band_file.GetGeoTransform()
+            x_arr = [0,x_size]
+            y_arr = [0,y_size]
+            extent = []
+            #determine extents
+            for xpixel in x_arr:
+                for ypixel in y_arr:
+                    x = geotrans[0]+(xpixel*geotrans[1])+(ypixel*geotrans[2])
+                    y = geotrans[3]+(xpixel*geotrans[4])+(ypixel*geotrans[5])
+                    extent.append([x,y])
+                y_arr.reverse()
+            """
+            slice file based on 100m squares
+            """
+            #get lower x, lower y
+            low_x=10000000000.0
+            low_y=10000000000.0
+            high_x=-10000000000.0
+            high_y=-10000000000.0
+            for pair in extent:
+                if pair[0]<low_x:
+                    low_x=pair[0]
+                if pair[0]>high_x:
+                    high_x=pair[0]
+                if pair[1]<low_y:
+                    low_y=pair[1]
+                if pair[1]>high_y:
+                    high_y=pair[1]
+            x_step = low_x
+            y_step = low_y
+            while x_step <= high_x:
+                while y_step <= high_y:
+                    cmd = self.generate_cmd_string(band['file'],self._satellite+'_'+band+'_'+self._acquisition_date+'_'+str(x_step)+'_'+str(y_step)+'.tif',x_step,y_step,100)
+                    os.system(cmd)
+                    """
+                    Store raster in database
+                    """
+                    """
+                    TODO: Implement psql insertion!
+                    """
+                    y_step+=100.0
+                y_step = low_y
+                x_step+=100.0
+            
+            
         
-        raise NotImplementedError
     def determine_raster_files(self,input_directory):
         """
         Parse the product metadata and determine the bands and masks
@@ -143,13 +219,13 @@ class Importer:
         
         
         
+        
     
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Import Landsat surface reflectance products.")
     parser.add_argument('--satellite', type=str, metavar='SATELLITE',required=True,choices=['7','8'],help="The product's satellite",dest="target_satellite")
     parser.add_argument('--dir',type=str,metavar='DIRECTORY',required=True,help="The path of the directory of SR products.",dest="directory")
     importer = Importer()
-    importer.get_satellite_dictionary[target_satellite]()
     importer.determine_raster_files(directory)
     importer.tile_raster()
     
